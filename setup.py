4e2b9f892c00b73fdb6f2a74d9d32a9c5f2de233
Davis <davis.yoshida@gmail.com>
2023-05-05 18:08:31 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name='jax_gptq',
+    version='0.0.1',
+    author='Davis Yoshida',
+    author_email='davis.yoshida@gmail.com',
+    description='JAX implementation of GPTQ',
+    long_description='.',
+    long_description_content_type='text',
+    url=None,
+    packages=['jax_gptq'],
+)
